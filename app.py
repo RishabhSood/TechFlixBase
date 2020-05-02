334da4d089b5f00b7fdd5c62c61472753e113e65
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2020-05-02 13:24:19 +0530

    Update app.py
    Added Leaderboard, Logout and checkpoint features. The implementation is complete.

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ Created on Fri May  1 11:14:06 2020
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_pymongo import PyMongo
 import hashlib
+import datetime
+import pymongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "***REMOVED***"
@@ -20,7 +22,8 @@ option_data = mongo.db.OPTIONS
 leaderboard = mongo.db.LEADERBOARD
 
 st = story_data.find_one({'s_id': '1'})
-qn = question_data.find_one({'q_id': '1'})
+qn = question_data.find_one({'q_id': '0'})
+op = option_data.find_one({'op_id': '0'})
 
 @app.route('/')
 def index():
@@ -53,7 +56,7 @@ def register():
             if request.form['password'] == request.form['confirm-password']:
                 uname = request.form['username']
                 hashpass = hashlib.sha256(request.form['password'].encode()).hexdigest()
-                Login.insert_one({'username': uname, 'password': hashpass, 'score': 0})
+                Login.insert_one({'username': uname, 'password': hashpass, 'score': 0, 'time': datetime.datetime.utcnow(), 's_id': '1'})
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
             else:
@@ -66,7 +69,9 @@ def register():
 @app.route('/storysection', methods=['GET', 'POST'])
 def display_story():
     global qn
+    global st
     user = Login.find_one({'username': session['username']})
+    st = story_data.find_one({'s_id': user['s_id']})
     score = user['score']
     story_sect = st['s_content']
     qn = question_data.find_one({'q_id': st['q_id']})
@@ -76,29 +81,64 @@ def display_story():
 
 @app.route('/question', methods=['GET', 'POST'])
 def display_question():
+    global op
     my_question = qn['question']
     user = Login.find_one({'username': session['username']})
     score = user['score']
+    op = option_data.find_one({'op_id': qn['op_id']})
     if request.method == 'POST':
         answer = request.form['answer']
         if answer == qn['answer']:
-            score += qn['points']
             user = Login.find_one({'username': session['username']})
-            Login.update_one(user, { "$set": { "score": score } })
+            Login.update_one(user, { "$set": {  "time": datetime.datetime.utcnow() } })
             return redirect(url_for('display_option'))
     return render_template('question.html', question = my_question, username = session['username'], score = score)
 
 @app.route('/option', methods=['GET', 'POST'])
 def display_option():
     global qn
+    global st
     user = Login.find_one({'username': session['username']})
     score = user['score']
+    
     if request.method == 'POST':
         option = request.form['options']
-        if option == 'option2':
-            #qn = Questions.query.filter(Questions.qid == '2').first()
+        if option == 'option1':
+            qn = question_data.find_one({'q_id': op['q_id']})
+            st = story_data.find_one({'s_id': op['s_id_1']})
+            score += qn['points']
+            Login.update_one(user, { "$set": { "score": score, "s_id": op['s_id_1']} })
             return redirect(url_for('display_story'))
-    return render_template('options.html', option_text = "pehla prasnhna ye rha aapki screen par", username = session['username'], score = score)
+        if option == 'option2':
+            qn = question_data.find_one({'q_id': op['q_id']})
+            st = story_data.find_one({'s_id': op['s_id_2']})
+            score += qn['points']
+            Login.update_one(user, { "$set": { "score": score, "s_id": op['s_id_2']} })
+            return redirect(url_for('display_story'))
+    return render_template('options.html', option_text = op['option_text'], option1 = op['opt_1'] , option2 = op['opt_2'] ,username = session['username'], score = score)
+
+@app.route('/leaderboard')
+def leaderboard():
+    leaderbd = Login.find().sort([("score",pymongo.DESCENDING), ("time",pymongo.ASCENDING)])
+    users_list = []
+    scores_list = []
+    rank_list = []
+    rank = 1
+    for x in leaderbd:
+        global user_rank
+        if x['username'] == session['username']:
+            user_rank = rank
+        users_list += [x['username']]
+        scores_list += [x['score']]
+        rank_list += [rank]
+        rank += 1
+    user = session['username']
+    return render_template("leaderboard.html", users = users_list, scores = scores_list, ranks = rank_list, truser = user, userrank = user_rank)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
