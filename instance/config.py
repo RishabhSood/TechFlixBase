e4dfd161b84e4fc58223aca125e860b5e07864ea
AulonSal <AulonSal@gmail.com>
2020-05-02 06:20:09 +0530

    Added some setup instructions

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-# Config
-
-MONGO_URI = 'mongodb://localhost:27017/TECHFLIX'
-SECRET_KEY = 'dev'
---
+++ setup/config.py
@@ -0,0 +1,7 @@
+# Config
+# Make an instance folder in the TechFlix directory, copy this file into that, configure as you wish, this config will
+# override the config in the python code.
+
+# This url can be used if you restore the datbase in setup/dump
+# MONGO_URI = 'mongodb://localhost:27017/TECHFLIX'
+SECRET_KEY = 'dev'
