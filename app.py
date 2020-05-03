bee548cc7c54d234511ad446aec68f13e0d097d3
AulonSal <AulonSal@gmail.com>
2020-05-03 12:08:02 +0530

    Made code pep8 compliant.

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ import datetime
 import pymongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "***REMOVED***"
+app.config["MONGO_URI"] = (
+    "***REMOVED***"
+)
 app.config['MONGO_DBNAME'] = "TECHFLIX_DB"
 mongo = PyMongo(app)
 Login = mongo.db.USER_DATA
@@ -25,12 +27,14 @@ st = story_data.find_one({'s_id': '1'})
 qn = question_data.find_one({'q_id': '0'})
 op = option_data.find_one({'op_id': '0'})
 
+
 @app.route('/')
 def index():
     if 'username' in session:
         return redirect(url_for('display_story'))
     
     return render_template('home.html')
+
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
@@ -42,13 +46,14 @@ def login():
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
             else:
-                return render_template('signin.html', alert = 'Invalid Username/Password')
+                return render_template('signin.html', alert='Invalid Username/Password')
             
-        return render_template('signin.html', alert = 'Invalid Username/Password')
+        return render_template('signin.html', alert='Invalid Username/Password')
     
-    return render_template('signin.html', alert = '')
+    return render_template('signin.html', alert='')
 
-@app.route('/register', methods = ['POST', 'GET'])
+
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         existing_user = Login.find_one({'username': request.form['username']})
@@ -56,15 +61,21 @@ def register():
             if request.form['password'] == request.form['confirm-password']:
                 uname = request.form['username']
                 hashpass = hashlib.sha256(request.form['password'].encode()).hexdigest()
-                Login.insert_one({'email': request.form['email'], 'username': uname, 'password': hashpass, 'score': 0, 'time': datetime.datetime.utcnow(), 's_id': '1'})
+                Login.insert_one({'email': request.form['email'],
+                                  'username': uname,
+                                  'password': hashpass,
+                                  'score': 0,
+                                  'time': datetime.datetime.utcnow(),
+                                  's_id': '1'})
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
             else:
-                return render_template('signup.html', alert = 'Passwords don\'t match!')
+                return render_template('signup.html', alert='Passwords don\'t match!')
         
-        return render_template('signup.html', alert = 'That username already exists!')
+        return render_template('signup.html', alert='That username already exists!')
     
-    return render_template('signup.html', alert = '')
+    return render_template('signup.html', alert='')
+
 
 @app.route('/storysection', methods=['GET', 'POST'])
 def display_story():
@@ -77,7 +88,8 @@ def display_story():
     qn = question_data.find_one({'q_id': st['q_id']})
     if request.method == 'POST':
         return redirect(url_for('display_question'))
-    return render_template('index.html', story_section = story_sect, username = session['username'], score = score)
+    return render_template('index.html', story_section=story_sect, username=session['username'], score=score)
+
 
 @app.route('/question', methods=['GET', 'POST'])
 def display_question():
@@ -90,9 +102,10 @@ def display_question():
         answer = request.form['answer']
         if answer == qn['answer']:
             user = Login.find_one({'username': session['username']})
-            Login.update_one(user, { "$set": {  "time": datetime.datetime.utcnow() } })
+            Login.update_one(user, {"$set": {"time": datetime.datetime.utcnow()}})
             return redirect(url_for('display_option'))
-    return render_template('question.html', question = my_question, username = session['username'], score = score)
+    return render_template('question.html', question=my_question, username=session['username'], score=score)
+
 
 @app.route('/option', methods=['GET', 'POST'])
 def display_option():
@@ -115,11 +128,13 @@ def display_option():
             score += qn['points']
             Login.update_one(user, { "$set": { "score": score, "s_id": op['s_id_2']} })
             return redirect(url_for('display_story'))
-    return render_template('options.html', option_text = op['option_text'], option1 = op['opt_1'] , option2 = op['opt_2'] ,username = session['username'], score = score)
+    return render_template('options.html', option_text=op['option_text'], option1=op['opt_1'], option2=op['opt_2'],
+                           username=session['username'], score=score)
+
 
 @app.route('/leaderboard')
 def leaderboard():
-    leaderbd = Login.find().sort([("score",pymongo.DESCENDING), ("time",pymongo.ASCENDING)])
+    leaderbd = Login.find().sort([("score", pymongo.DESCENDING), ("time", pymongo.ASCENDING)])
     users_list = []
     scores_list = []
     rank_list = []
@@ -133,12 +148,15 @@ def leaderboard():
         rank_list += [rank]
         rank += 1
     user = session['username']
-    return render_template("leaderboard.html", users = users_list, scores = scores_list, ranks = rank_list, truser = user, userrank = user_rank)
+    return render_template("leaderboard.html", users=users_list, scores=scores_list,
+                           ranks=rank_list, truser=user, userrank=user_rank)
+
 
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
