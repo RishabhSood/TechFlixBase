971915fbfa556157e485bf8a50921602f097f93d
AulonSal <AulonSal@gmail.com>
2020-05-05 03:28:10 +0530

    Auth at par with master

--- techflix/auth.py
+++ techflix/auth.py
@@ -2,6 +2,8 @@ import datetime
 import functools
 import hashlib
 
+# TODO: Implement method to enter debugger from any page
+# TODO: Clear loophole where you can load questions and then open options to go further
 from flask import (
     Blueprint, render_template, redirect, url_for, request, session, g,
 )
@@ -16,16 +18,25 @@ def register():
     if request.method == 'POST':
         # TODO: Check for empty userstring/password string
         username = request.form['username']
-        # raise ValueError  # d
 
-        # Checking for preexisting user
+        # Validating form input
+        # Checking for empty/no username
+        if not username:
+            return render_template('register.html', alert="Username Required")
+
+        # # Why don't I store the password in a variable? Ineptitude derived security paranoia
+        # Checking for empty/no password
+        if not request.form['password']:
+            return render_template('register.html', alert="Password Required")
+
+        # Checking for username being taken
         existing_user = users.find_one({'username': username})
         if existing_user:
-            return render_template('register.html', alert='That username already exists!')
+            return render_template('register.html', alert="That username already exists!")
 
-        # Checking for password conflict
+        # Checking for conflicting password entries
         if request.form['password'] != request.form['confirm-password']:
-            return render_template('register.html', alert='Passwords don\'t match!')
+            return render_template('register.html', alert="Passwords don't match!")
 
         hashed_password = hash_password(request.form['password'])
 
@@ -49,22 +60,24 @@ def register():
 def login():
     from .database import users
 
+    username = request.form['username']
+
     if request.method == 'POST':
         user = users.find_one({'username': request.form['username']})
-        if user is not None:
-            hash_ = hashlib.sha256(request.form['password'].encode()).hexdigest()
-            if user['password'] == hash_:
-                session['username'] = request.form['username']
-                return redirect(url_for('index'))
-            else:
-                return render_template('login.html', alert='Invalid Username/Password')
+        if not user:
+            return render_template('login.html', alert='Invalid Username')
 
-        return render_template('login.html', alert='Invalid Username/Password')
+        # # I might have refactored to an antipattern here
+        hashed_password = hash_password(request.form['password'])
+        if user['password'] != hashed_password:
+            return render_template('login.html', alert='Invalid Password')
+
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
 
     return render_template('login.html', alert='')
 
 
-# Temporary login I needed
 @bp.route('/logout')
 def logout():
     session.clear()
@@ -83,6 +96,6 @@ def login_required(view):
     return wrapped_view
 
 
-# Password hashing function used
+# Password hashing function
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
