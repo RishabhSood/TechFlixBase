3bbad00c4777c5a6147646506356927bbbaee648
Stark <55579405+Udbhav18@users.noreply.github.com>
2020-05-22 00:10:43 +0530

    Add files via upload
    instance folder

---
+++ instance/config.py
@@ -0,0 +1,9 @@
+# Config
+# Make an instance folder in the TechFlix directory, copy this file into that, configure as you wish, this config will
+# override the config in the python code.
+
+# Use this to override the hardcoded uri
+# MONGO_URI =
+
+# Obviously replace this in production
+SECRET_KEY = 'dev'
